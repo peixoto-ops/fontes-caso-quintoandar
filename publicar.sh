214da7d12666024901e389769e70c0a405072582
peixoto-ops <peixoto.ops@gmail.com>
2025-11-20 11:21:25 -0300

    Update: Coleção Sentença Arbitral - Caso Quinto Andar atualizada em 20/11/2025 11:21

--- publicar.sh
+++ publicar.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# --- CONFIGURAÇÃO ---
-# Caminho da sua pasta de dados do Zotero
-ZOTERO_DATA="$HOME/Zotero"
-# Nome da coleção específica (se quiser todas, remova a flag --collection abaixo)
-COLECAO="Sentença Arbitral - Caso Quinto Andar"
-
-echo "--- 1. Ativando Ambiente Virtual ---"
-source venv/bin/activate
-
-echo "--- 2. Gerando Site Estático (Zotsite) ---"
-# O output vai para a pasta 'docs' para o GitHub Pages ler
-# Se for exportar TUDO, remova: --collection "$COLECAO"
-zotsite export --data "$ZOTERO_DATA" --output docs --collection "$COLECAO"
-
-echo "--- 3. Enviando para o GitHub ---"
-git add .
-git commit -m "Update: Fontes atualizadas em $(date '+%d/%m/%Y %H:%M')"
-git push origin main
-
-echo "--- FEITO! ---"
-echo "Acesse em: https://peixoto-ops.github.io/fontes-processuais/"#!/bin/bash
-
 # --- CONFIGURAÇÕES ---
 # 1. Caminho do banco de dados (Padrão do Ubuntu)
 ZOTERO_DB="$HOME/Zotero"
